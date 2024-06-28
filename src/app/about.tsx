81a9b4130ff98200c1749326bb7df906fbb164b2
vijay.dhanvai@noborderz.com <vijay.dhanvai@noborderz.com>
2024-06-28 11:02:15 +0530

    Updated

---
+++ src/app/about.tsx
@@ -0,0 +1,16 @@
+import "./style.scss";
+
+export default function About() {
+  return (
+    <main className="main">
+      
+      <div className="container">
+        <div className="row">
+          <h1>About Page</h1>
+        </div>
+      </div>
+
+    </main>
+
+    );
+}
